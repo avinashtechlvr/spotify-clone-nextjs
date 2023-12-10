391ca1a99da9ee84ca3ad07a34cfb0b3619f9212
avinashtechlvr <avinashiniiitdwd@gmail.com>
2023-12-10 10:08:07 +0530

    Toast for logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import { RxCaretLeft, RxCaretRight} from "react-icons/rx";
-import {HiHome} from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
+
 import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
 
@@ -12,17 +17,29 @@ interface HeaderProps {
     className?: string;
 }
 const Header: React.FC<HeaderProps> = ({
-    children, 
+    children,
     className
 }) => {
     const router = useRouter();
     const authModal = useAuthModal();
-    const handleLogout = () => {
-        // handle logout feature
+
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
+
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+
+        // reset any playing songs
+        router.refresh();
+        if (error) {
+            toast.error(error.message);
+        } else {
+            toast.success('Logged Out');
+        }
     }
     return (
         <div
-        className={twMerge(`
+            className={twMerge(`
         h-fit
         bg-gradient-to-b
         from-emerald-800
@@ -38,50 +55,67 @@ const Header: React.FC<HeaderProps> = ({
             ">
                 <div className="hidden md:flex gap-x-2 items-center">
                     <button className=" rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
-                    onClick={() => router.back()} 
+                        onClick={() => router.back()}
                     >
-                        <RxCaretLeft size={35}  className="tex-white" />
+                        <RxCaretLeft size={35} className="tex-white" />
                     </button>
                     <button className=" rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
-                    onClick={() => router.forward()}
+                        onClick={() => router.forward()}
                     >
-                        <RxCaretRight size={35}  className="tex-white" />
+                        <RxCaretRight size={35} className="tex-white" />
                     </button>
                 </div>
                 <div className="flex md:hidden gap-x-2 items-center">
                     <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
-                        <HiHome className="text-black" size={20} /> 
+                        <HiHome className="text-black" size={20} />
                     </button>
                     <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
-                        <BiSearch className="text-black" size={20} /> 
+                        <BiSearch className="text-black" size={20} />
                     </button>
 
                 </div>
                 <div className="flex justify-between items-center gap-x-4">
-                    <>
-                        <div>
-                            <Button className="
+                    {user ?
+                        (
+                            <div className="flex gap-x-4 items-center">
+                                <Button
+                                    onClick={handleLogout}
+                                    className="bg-white px-6 py-2"
+                                >
+                                    Logout
+                                </Button>
+                                <Button
+                                    onClick={() => router.push('/account')}
+                                    className="bg-white"
+                                >
+                                    <FaUserAlt />
+                                </Button>
+                            </div>
+                        ) :
+                        (<>
+                            <div>
+                                <Button className="
                             bg-transparent
                             text-neutral-300
                             font-medium
-                            " 
-                            onClick={authModal.onOpen}
-                            >Sign up 
-                            </Button>
-                        </div>
-                        <div>
-                            <Button className="
+                            "
+                                    onClick={authModal.onOpen}
+                                >Sign up
+                                </Button>
+                            </div>
+                            <div>
+                                <Button className="
                             bg-white
                             px-6
                             py-3
-                            " 
-                            onClick={authModal.onOpen}
+                            "
+                                    onClick={authModal.onOpen}
 
-                            >
-                                Login
-                            </Button>
-                        </div>
-                    </>
+                                >
+                                    Login
+                                </Button>
+                            </div>
+                        </>)}
                 </div>
             </div>
             {children}
