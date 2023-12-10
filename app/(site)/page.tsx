1a364af8af4371589afc7f6c959993603ef5103e
avinashtechlvr <avinashiniiitdwd@gmail.com>
2023-12-10 12:19:36 +0530

    Functionality to fetch all songs from db

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
 import Header from '@/components/Header'
 import ListItem from '@/components/ListItem'
+import getSongs from '@/actions/getSongs';
+import PageContent from '@/components/PageContent';
 
 export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className='
     bg-neutral-900
@@ -30,9 +32,7 @@ export default function Home() {
             Newest Songs
           </h1>
         </div>
-        <div>
-          List of Songs!
-        </div>
+        <PageContent />
       </div>
     </div>
   )
---
+++ components/PageContent.tsx
@@ -0,0 +1,11 @@
+
+
+const PageContent = () => {
+    return(
+        <div>
+           PageContent
+        </div>
+    )
+}
+
+export default PageContent;
