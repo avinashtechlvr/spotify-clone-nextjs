e107c664525aebeb9133445b3e7d3dc5589ba35f
avinashtechlvr <avinashiniiitdwd@gmail.com>
2023-12-10 15:30:48 +0530

    Like or dislike songs in search

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
@@ -24,7 +25,7 @@ const SearchContent:React.FC<SearchContentProps> = ({
                     <div className="flex-1">
                         <MediaItem  onClick={() => {}} data={song}/>
                     </div>
-                    {/* TODO: Add Like Button Here */}
+                    <LikeButton songId={song.id} />
                 </div>
             ))}
         </div>
