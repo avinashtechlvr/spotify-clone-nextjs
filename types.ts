ab8ea9f69534c17ee65bd7ba7df0dc28a1de6a85
avinashtechlvr <avinashiniiitdwd@gmail.com>
2023-12-25 13:40:02 +0530

    Integrated Webhooks for subscription model

--- types.ts
+++ types.ts
@@ -36,7 +36,7 @@ export interface Price{
     type?: Stripe.Price.Type;
     interval?: Stripe.Price.Recurring.Interval;
     interval_count?: number;
-    trail_period_days?: number | null;
+    trial_period_days?: number | null;
     metadata?: Stripe.Metadata;
     products?: Product;
 }
